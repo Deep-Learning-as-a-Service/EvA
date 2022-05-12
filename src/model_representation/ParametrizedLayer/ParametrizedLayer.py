656086eb20eafcf6676817419826c960934db117
leander.masopust <leander.masopust@gmail.com>
2022-05-12 14:35:18 +0200

    add probabilities for layer param mutation intensity depending on layer mutation intensity

--- src/model_representation/ParametrizedLayer/ParametrizedLayer.py
+++ src/model_representation/ParametrizedLayer/ParametrizedLayer.py
@@ -1,8 +1,35 @@
 from abc import ABC, abstractmethod 
 import keras.layers
+import random
 
 class ParametrizedLayer(ABC):
     innovation_number = 0
+    intensity_to_param_mutation_probability = {
+        "low": {
+            "low" : 0.8,
+            "mid" : 0.2,
+            "high" : 0.0,
+            "all" : 0.0
+        },
+        "mid": {
+            "low" : 0.3,
+            "mid" : 0.4,
+            "high" : 0.3,
+            "all" : 0.0
+        },
+        "high": {
+            "low" : 0.0,
+            "mid" : 0.3,
+            "high" : 0.4,
+            "all" : 0.3
+        },
+       "all": {
+            "low" : 0.0,
+            "mid" : 0.0,
+            "high" : 0.0,
+            "all" : 1.0
+        }
+    }
     def __init__(self, params) -> None:
         self.params = params
         self.innovation_number = ParametrizedLayer.innovation_number
@@ -16,9 +43,10 @@ class ParametrizedLayer(ABC):
         # lambda x: self.layer(**kwargs)(x)
         return self._layer(**kwargs)
     
-    def mutate(self, intensity):
+    def mutate(self, layer_mutation_intensity):
         for param in self.params:
-            param.mutate(intensity)
+            param_mutation_intensity = ParametrizedLayer.get_param_mutation(layer_mutation_intensity)
+            param.mutate(param_mutation_intensity)
         self.innovation_number = ParametrizedLayer.innovation_number
         ParametrizedLayer.innovation_number += 1
     
@@ -49,3 +77,16 @@ class ParametrizedLayer(ABC):
         params = [param_class.create_random_default() for param_class in cls._param_classes]
         return cls(params=params)
     
+    @classmethod
+    def get_param_mutation(cls, intensity) -> str:
+        rand_dict = cls.intensity_to_param_mutation_probability[intensity]
+        assert sum(rand_dict.values()) == 1.0, "sum of probabilities should be 1"
+        rand_number = random.random()
+        mutation: str = None
+        endProbability = 0.0
+        for mutation_type, mutation_type_probability in rand_dict.items():
+            endProbability += mutation_type_probability
+            if rand_number <= endProbability:
+                mutation = mutation_type
+                break
+        return mutation
