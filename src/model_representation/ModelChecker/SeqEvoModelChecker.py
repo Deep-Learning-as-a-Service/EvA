3f53d9215694ca923e416636c2ad052a8e3a6557
leander.masopust <leander.masopust@gmail.com>
2022-05-15 21:53:36 +0200

    another conv removal fix

--- src/model_representation/ModelChecker/SeqEvoModelChecker.py
+++ src/model_representation/ModelChecker/SeqEvoModelChecker.py
@@ -72,9 +72,12 @@ class SeqEvoModelChecker():
 
         # remove conv layer and reset parents/childrens of adjacent layers
         seqevo_genome.layers.pop(layer_idx_to_remove)
-        if layer_idx_to_remove - 1 >= 0:
+        if layer_idx_to_remove - 1 >= 0 and layer_idx_to_remove + 1 < len(seqevo_genome.layers):
             seqevo_genome.layers[layer_idx_to_remove - 1].childs = [seqevo_genome.layers[layer_idx_to_remove + 1]]
-        if layer_idx_to_remove + 1 < len(seqevo_genome.layers):
             seqevo_genome.layers[layer_idx_to_remove + 1].parents = [seqevo_genome.layers[layer_idx_to_remove - 1]]
+        elif layer_idx_to_remove - 1 >= 0:
+            seqevo_genome.layers[layer_idx_to_remove - 1].childs = None
+        elif layer_idx_to_remove + 1 < len(seqevo_genome.layers):
+            seqevo_genome.layers[layer_idx_to_remove + 1].parents = None
 
 
