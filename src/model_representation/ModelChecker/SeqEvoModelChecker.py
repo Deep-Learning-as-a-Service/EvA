f282ef93925e85fe64faeff73c71156b7ae4c39e
leanma <leander.masopust@gmail.com>
2022-05-25 12:02:45 +0200

    modelchecker now also takes channel dimension into calcs and checks for compatibility there

--- src/model_representation/ModelChecker/SeqEvoModelChecker.py
+++ src/model_representation/ModelChecker/SeqEvoModelChecker.py
@@ -15,26 +15,51 @@ class SeqEvoModelChecker():
     @classmethod
     def fix_convolution_dimension_loss(cls, seqevo_genome: SeqEvoGenome):
         # TODO: get those magic numbers from data (globally accessible?)
+        def problematic_input(timesteps_dimension_size_after_convs, features_dimension_size_after_convs, channel_dimension_size_after_convs) -> bool:
+            if channel_dimension_size_after_convs:
+                if channel_dimension_size_after_convs <= 0:
+                    return True
+            if timesteps_dimension_size_after_convs <= 0 or features_dimension_size_after_convs <= 0:
+                return True
+            return False
+        
         timesteps_dimension_size = 90
         features_dimension_size = 51
-        timesteps_dimension_size_after_convs, features_dimension_size_after_convs = cls.__calculate_dimensions_after(timesteps_dimension_size, features_dimension_size, seqevo_genome.layers)
-        while(timesteps_dimension_size_after_convs <= 0 or features_dimension_size_after_convs <= 0):
+        timesteps_dimension_size_after_convs, features_dimension_size_after_convs, channel_dimension_size_after_convs = cls.__calculate_dimensions_after(timesteps_dimension_size, features_dimension_size, seqevo_genome.layers)
+
+        while(problematic_input(timesteps_dimension_size_after_convs, features_dimension_size_after_convs, channel_dimension_size_after_convs)):
             cls.__remove_random_conv_layer(seqevo_genome)
-            timesteps_dimension_size_after_convs, features_dimension_size_after_convs = cls.__calculate_dimensions_after(timesteps_dimension_size, features_dimension_size, seqevo_genome.layers)
+            timesteps_dimension_size_after_convs, features_dimension_size_after_convs, channel_dimension_size_after_convs = cls.__calculate_dimensions_after(timesteps_dimension_size, features_dimension_size, seqevo_genome.layers)
 
     @classmethod
-    def __calculate_dimensions_after(cls, timesteps_dimension_size, features_dimension_size, layers) -> 'tuple[int, int]':
+    def __calculate_dimensions_after(cls, timesteps_dimension_size, features_dimension_size, layers) -> 'tuple[int, int, int]':
         timesteps_dimension_size_after_convs = timesteps_dimension_size
         features_dimension_size_after_convs = features_dimension_size
+        channel_dimension_size_after_convs = None
 
         for layer in layers:
-            
             # if dimension loss in the middle of model => return -1, -1 to trigger resolver
-            if(timesteps_dimension_size_after_convs <= 0 or features_dimension_size_after_convs <= 0):
-                return -1, -1
+            if (timesteps_dimension_size_after_convs <= 0 or features_dimension_size_after_convs <= 0):
+                return -1, -1, -1
+            if channel_dimension_size_after_convs:
+                if channel_dimension_size_after_convs <= 0:
+                    return -1, -1, -1
             
             # size of features dimension gets reduced to "units" in LSTM/Dense
-            if layer.__class__.__name__ in ["PLstmLayer", "PDenseLayer"]:
+            if layer.__class__.__name__ == "PDenseLayer":
+                units = None
+                for param in layer.params:
+                    if param._key == "units":
+                        units = param.value
+                if channel_dimension_size_after_convs:
+                    channel_dimension_size_after_convs = units
+                else:
+                    features_dimension_size_after_convs = units
+                    
+            if layer.__class__.__name__ == "PLstmLayer":
+                if channel_dimension_size_after_convs:
+                    features_dimension_size_after_convs *= channel_dimension_size_after_convs
+                    channel_dimension_size_after_convs = None
                 units = None
                 for param in layer.params:
                     if param._key == "units":
@@ -45,31 +70,39 @@ class SeqEvoModelChecker():
             if layer.__class__.__name__ == "PConv1DLayer":
                 kernel_size = None
                 stride = None
+                filters = None
                 for param in layer.params:
                     if param._key == "kernel_size":
                         kernel_size = param.value
                     if param._key == "strides":
                         stride = param.value  
+                    if param._key == "filters":
+                        filters = param.value
+                channel_dimension_size_after_convs = filters
                 timesteps_dimension_size_after_convs = math.floor((timesteps_dimension_size_after_convs - kernel_size) / stride) + 1
 
             # size of timesteps and features dimension gets reduced by Conv2D
             elif layer.__class__.__name__ == "PConv2DLayer":
                 kernel_size = None
                 stride = None
+                filters = None
                 for param in layer.params:
                     if param._key == "kernel_size":
                         kernel_size = param.value
                     if param._key == "strides":
                         stride = param.value 
-
+                    if param._key == "filters":
+                        filters = param.value
                 timesteps_kernel_size = kernel_size[0]
                 features_kernel_size = kernel_size[1]
                 timesteps_stride = stride[0]
                 features_stride = stride[1]
+                
                 timesteps_dimension_size_after_convs = math.floor((timesteps_dimension_size_after_convs - timesteps_kernel_size) / timesteps_stride) + 1
                 features_dimension_size_after_convs = math.floor((features_dimension_size_after_convs - features_kernel_size) / features_stride) + 1
+                channel_dimension_size_after_convs = filters
 
-        return timesteps_dimension_size_after_convs, features_dimension_size_after_convs
+        return timesteps_dimension_size_after_convs, features_dimension_size_after_convs, channel_dimension_size_after_convs
 
     @classmethod
     def __remove_random_conv_layer(cls, seqevo_genome) -> None:
