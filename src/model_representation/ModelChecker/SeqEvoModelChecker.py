64ce64f7234fdf351fea9ddddb384154247c95af
leanma <leander.masopust@gmail.com>
2022-06-20 16:21:26 +0200

    prevent OOM error

--- src/model_representation/ModelChecker/SeqEvoModelChecker.py
+++ src/model_representation/ModelChecker/SeqEvoModelChecker.py
@@ -15,32 +15,76 @@ class SeqEvoModelChecker():
 
     @classmethod
     def check_model_genome(cls, seqevo_genome: SeqEvoGenome) -> None:
-        cls.fix_convolution_dimension_loss(seqevo_genome)
-        cls.alter_models_with_high_params(seqevo_genome)
+        cls.fix_convolution_dimension_loss(seqevo_genome) # make model compilable 
+        cls.fix_memory_overflow(seqevo_genome) # make model compilable 
+        cls.alter_models_with_high_params(seqevo_genome) # reduce time expenditure
         
     @classmethod
     def alter_models_with_high_params(cls, seqevo_genome: SeqEvoGenome):
-        threshold = 20000000 # 20 mil params as threshold, subject of change
+        threshold = 10000000 # 10 mil params as threshold, subject of change
         
         while True:
             model_genome = SeqEvoModelGenome.create_with_default_params(seqevo_genome)
-            
             keras_model = model_genome.get_model()
             num_params = keras_model.count_params()
-       
             if num_params <= threshold:
                 break
-            
             else:
                 cls.alter_high_param_layers(seqevo_genome)
-
     
     @classmethod
-    def alter_high_param_layers(cls, seqevo_genome: SeqEvoGenome):
+    def fix_memory_overflow(cls, seqevo_genome: SeqEvoGenome):
+        # TODO: get batch size from settings as well
+        batch_size = 32
+        threshold = pow(2, 24) # if number of neurons in any layer is higher than threshold, reduce big layers
+        
+        # reduce layer params while GPU would be OOM when compiling
+        while True:
+            timesteps_dimension_size_after_convs = settings.data_dimension_dict["window_size"]
+            features_dimension_size_after_convs = settings.data_dimension_dict["n_features"]
+            channel_dimension_size_after_convs = 1
+            too_large = False
+            too_large_idx = None
+            # calculate number of neurons used by each layer
+            for idx, layer in enumerate(seqevo_genome.layers):
+                timesteps_dimension_size_after_convs, features_dimension_size_after_convs, channel_dimension_size_after_convs = cls.__calculate_dimensions_after(timesteps_dimension_size_after_convs, features_dimension_size_after_convs, [layer], channel_dimension_size_after_convs)
+                
+                # channel size can be none, set to 1 for calculation afterwards
+                if not channel_dimension_size_after_convs:
+                    channel_dimension_size_after_convs = 1
+                total_neurons = timesteps_dimension_size_after_convs * features_dimension_size_after_convs * channel_dimension_size_after_convs * batch_size
+                
+                # if total neurons is too large => break and reduce layer params
+                if total_neurons >= threshold:
+                    too_large = True
+                    too_large_idx = idx
+                    break
+                
+            if not too_large:
+                break 
+            
+            cls.alter_high_param_layers_before_idx(seqevo_genome, too_large_idx)
+                
+    @classmethod
+    def alter_high_param_layers_before_idx(cls, seqevo_genome: SeqEvoGenome, idx: int):
+        """
+        try "alter_high_param_layers" before given idx, if there is no dense/lstm before given idx => remove random layer before given idx
+        """     
+        high_param_layer_idx_list = list(filter(lambda idx: (seqevo_genome.layers[idx].__class__.__name__ in ["PDenseLayer", "PLstmLayer"]), range(idx)))
+        if len(high_param_layer_idx_list) == 0:
+            seqevo_genome.layers.pop(random.randrange(idx+1))
+        else:
+            cls.alter_high_param_layers(seqevo_genome, before_idx=idx+1)
+
+    @classmethod
+    def alter_high_param_layers(cls, seqevo_genome: SeqEvoGenome, before_idx=None):
         """
         find Dense/LSTM layer with highest units number and change to a fifth of original units or minimum of value range  
         """
-        high_param_layer_idx_list = list(filter(lambda idx: (seqevo_genome.layers[idx].__class__.__name__ in ["PDenseLayer", "PLstmLayer"]), range(len(seqevo_genome.layers))))
+        max_idx_to_alter = len(seqevo_genome.layers)
+        if before_idx:
+            max_idx_to_alter = before_idx
+        high_param_layer_idx_list = list(filter(lambda idx: (seqevo_genome.layers[idx].__class__.__name__ in ["PDenseLayer", "PLstmLayer"]), range(max_idx_to_alter)))
         highest_n_units = 0
         highest_n_units_idx = 0
         
@@ -83,8 +127,8 @@ class SeqEvoModelChecker():
                 return True
             return False
         
-        timesteps_dimension_size = 90
-        features_dimension_size = 51
+        timesteps_dimension_size = settings.data_dimension_dict["window_size"]
+        features_dimension_size = settings.data_dimension_dict["n_features"]
         timesteps_dimension_size_after_convs, features_dimension_size_after_convs, channel_dimension_size_after_convs = cls.__calculate_dimensions_after(timesteps_dimension_size, features_dimension_size, seqevo_genome.layers)
 
         while(problematic_input(timesteps_dimension_size_after_convs, features_dimension_size_after_convs, channel_dimension_size_after_convs)):
@@ -92,10 +136,10 @@ class SeqEvoModelChecker():
             timesteps_dimension_size_after_convs, features_dimension_size_after_convs, channel_dimension_size_after_convs = cls.__calculate_dimensions_after(timesteps_dimension_size, features_dimension_size, seqevo_genome.layers)
 
     @classmethod
-    def __calculate_dimensions_after(cls, timesteps_dimension_size, features_dimension_size, layers) -> 'tuple[int, int, int]':
+    def __calculate_dimensions_after(cls, timesteps_dimension_size, features_dimension_size, layers, channel_dimension_size=None) -> 'tuple[int, int, int]':
         timesteps_dimension_size_after_convs = timesteps_dimension_size
         features_dimension_size_after_convs = features_dimension_size
-        channel_dimension_size_after_convs = None
+        channel_dimension_size_after_convs = channel_dimension_size
 
         for layer in layers:
             # if dimension loss in the middle of model => return -1, -1 to trigger resolver
@@ -192,13 +236,10 @@ class SeqEvoModelChecker():
         for layer_idx in conv_layer_idx_list:
             layer = seqevo_genome.layers[layer_idx]
             kernel_size = None
-            stride = None
 
             for param in layer.params:
                 if param._key == "kernel_size":
                     kernel_size = param.value
-                if param._key == "strides":
-                    stride = param.value  
             if layer.__class__.__name__ == "PConv1DLayer":
                 if kernel_size > highest_kernel_size:
                     highest_kernel_size = kernel_size
@@ -223,13 +264,3 @@ class SeqEvoModelChecker():
         stride_size_param = [x for x in params if x._key == "strides"][0]
         params[kernel_size_param_idx] = new_kernel_param
         stride_size_param._dependent_on_param = new_kernel_param
-        
-            
-            
-
-
-            
-
-    
-    
-
